e6d50b0f0ddcd5d5853209057e38ca2b0aabb7d4
smjawhn <84219257+smjawhn@users.noreply.github.com>
2025-09-01 21:09:23 +0900

    월요일 저녁

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -7,7 +7,6 @@ public class UIManager : MonoBehaviour
 {
     public static UIManager Instance { get; private set; }
     public WarningPopup warningPopup;
-    public TMP_Text goldText;
 
     private void Awake()
     {
@@ -18,6 +17,26 @@ public class UIManager : MonoBehaviour
         }
         Instance = this;
         DontDestroyOnLoad(gameObject);
+    }
+
+    private void OnEnable()
+    {
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.OnSpendFailed += HandleSpendFailed;
+        }
+    }
+    private void OnDisable()
+    {
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.OnSpendFailed -= HandleSpendFailed;
+        }
+    }
+
+    private void HandleSpendFailed(GameObject popup)
+    {
+        popup.SetActive(true);
     }
 
 }
