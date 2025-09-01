e8fc753cf9ac8cf30518ee56d60dbd456daa6176
hswhouse <hswhouse@gmail.com>
2025-09-01 20:22:30 +0900

    enemy.add [애니메이션 추가]

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -11,21 +11,35 @@ public class Enemy : MonoBehaviour
     public float Nowhealth = 100.0f;//슬라임을 생성할 때 마다 최대 체력과 같게 설정하게 바꿈
     public float Hitdamage = 5; //이 값은 나중에 공격력을 받아오는걸로 교체
 
-    private Animator animator;
+
+    [SerializeField] private Animator animator;
+    private static readonly int HashHit = Animator.StringToHash("Hit");
+    private static readonly int HashDie = Animator.StringToHash("Die");
+
+    private bool isDying;
+
     public void Update() // 클릭 이벤트에서 대채함 지금은 임시 테스트용
+
     {
+        if (isDying)return;
+
+
         if (Input.GetMouseButtonDown(0))
         {
+
             IsDamaged();
 
         }
     }
     public void IsDamaged()
     {
+        if (isDying) return;
         Nowhealth = Nowhealth - Hitdamage;
         if (Nowhealth > 0)
         {
+
             front.localScale = new Vector3(Nowhealth / Fullhealth, 1.0f);
+            animator.SetTrigger(HashHit);
         }
         else
         {
@@ -36,8 +50,10 @@ public class Enemy : MonoBehaviour
     }
     public void IsDie()
     {
+        if (isDying)return;
+
+        isDying = true;
+        animator.SetTrigger(HashDie);
         Debug.Log("으앙주금");
     }
-
-
 }
