27777647e3717a478d5da238a2de44dd071a312f
DMDZ99 <ldhdo430@naver.com>
2025-09-01 16:15:54 +0900

    무기데이터테이블

---
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] private WeaponStats statData;
+    private int level;
+
+    public void Enforce()
+    {
+        level++;
+        if (level > 5)
+            level = 5;
+        var stat = statData.GetEnforceLevel(level);
+
+        Debug.Log("강화완료");
+    }
+}
---
+++ Assets/Scripts/Weapon/WeaponStats.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WeaponStatData", menuName = "GameData/WeaponStatData")]
+public class WeaponStats : ScriptableObject
+{
+    public EnforceLevel[] stats;    // 강화별 스탯
+
+    public EnforceLevel GetEnforceLevel(int level)  // 강화레벨정보 가져오기
+    {
+        if (level > 5) level = 5;
+
+        return stats[level];    // 0강부터 5강까지
+    }
+}
